9191051bf1f4fc1fb4f3cfc36fd3d1c6a8918a04
sina80 <sinakhadem.f@gmail.com>
2024-09-11 12:55:18 -0700

    done

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,13 +4,10 @@ from ecommerce import views
 
 urlpatterns = [
 
-    path('',views.store,name='store'),
-    path('cart/',views.cart,name='cart'),
-    path('checkout/',views.checkout,name='checkout'),
+    path('', views.store, name='store'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
 
-    path('update_item/',views.updateItem,name='update_item'),
-
-
+    path('update_item/', views.updateItem, name='update_item'),
 
 ]
-
